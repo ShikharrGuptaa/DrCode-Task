8444b7556a08d909255cba860a40aa6c47c32b75
Ritesh <111880955+RiteshRajMoond@users.noreply.github.com>
2025-05-30 19:38:46 +0530

    Delete Desktop directory

--- Desktop/xeno-crm/server/utils/AppError.util.js
+++
@@ -1,8 +0,0 @@
-export class AppError extends Error {
-  constructor(message, statusCode) {
-    super(message);
-    this.statusCode = statusCode;
-    this.success = false;
-    Error.captureStackTrace(this, this.constructor);
-  }
-}
